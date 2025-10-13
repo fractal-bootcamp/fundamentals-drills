9c44ac65ca33131169a5644fa8891ebafd6d543b
Ethan Anderson <150651098+mite404@users.noreply.github.com>
2025-10-13 11:56:00 -0400

    Puzzle problem `assignment1.ts` commit

--- puzzles/problems/assignment1.ts
+++ puzzles/problems/assignment1.ts
@@ -15,5 +15,32 @@
  */
 
 export function findTopStudent(students): string {
-  return "TODO"
+  // if students' scores array is empty
+  // if (students.scores === []) return console.log('Students scores array is empty')
+
+  // find the average of each student's scores
+  const student1 = students[0]
+
+  const student2 = students[1]
+
+  for (let i = 0; i < student1.scores.length; i++) {
+    const stu1avg = student1.scores / student1.scores.length
+    const studentAvg1String = stu1avg.toString()
+    return studentAvg1String
+  }
+
+  for (let i = 0; i < student2.scores.length; i++) {
+    const stu2avg = student2.scores / student2.length
+    const studentAvg2String = stu2avg.toString()
+    // return studentAvg2String
+  }
+
+  // compare the two averages and return the name of student w/ highest avg.
+  if (studentAvg1String > studentAvg2String) {
+    return student1.name
+  } else if (studentAvg1String < studentAvg2String) {
+    return student2.name
+  } else (studentAvg1String === studentAvg2String) {  // if tie return student who's name appears first in array
+    return student1[0].name
+  }
 }
