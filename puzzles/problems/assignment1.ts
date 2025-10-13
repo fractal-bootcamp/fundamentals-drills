e99e670a58ae8c2147d322f9b6bd60e7c137004e
luebbertevan <luebbertevan@gmail.com>
2025-10-13 11:58:36 -0400

    puzzle

--- puzzles/problems/assignment1.ts
+++ puzzles/problems/assignment1.ts
@@ -14,6 +14,32 @@
  * - [{ name: "Charlie", scores: [80] }, { name: "Dana", scores: [80, 80] }] → "Charlie"
  */
 
-export function findTopStudent(students): string {
-  return "TODO"
+function findTestAvg(scores: number[]): number {
+	let sum = 0;
+	for (const score of scores) {
+		sum += score;
+	}
+	console.log(sum / scores.length);
+	return sum / scores.length;
+}
+
+type student = {
+	name: string;
+	scores: number[];
+};
+
+export function findTopStudent(students: student[]): string {
+	if (students.length == 0) throw Error("No students in class");
+	let topStudent = students[0].name;
+	let topScore = 0;
+	for (const student of students) {
+		console.log(findTestAvg(student.scores));
+		if (findTestAvg(student.scores) > topScore) {
+			//???? Idk why this is evaluating to true
+			console.log("setting top", student.name);
+			topStudent = student.name;
+		}
+		//console.log("student: ", student.name, findTestAvg(student.scores));
+	}
+	return topStudent;
 }
