2a08213c087e9723fbefb6215246c9ee379cc465
rpasetes <russap01@gmail.co>
2025-10-13 11:55:56 -0400

    puzzles first pass

--- puzzles/problems/assignment1.ts
+++ puzzles/problems/assignment1.ts
@@ -14,6 +14,33 @@
  * - [{ name: "Charlie", scores: [80] }, { name: "Dana", scores: [80, 80] }] → "Charlie"
  */
 
-export function findTopStudent(students): string {
-  return "TODO"
+type Student = {
+  name: string;
+  scores: number[];
+}
+
+type StudentAverage = {
+  name: string;
+  average: number;
+}
+
+// return StudentAverage.name of StudentAverage.average.max()
+// (1133) will probably do a reduce? here on StudentAverage[], mdn bb
+// (1137) ffref: how to do type checking on consts?
+// (1142) Student[] => StudentAverage[] => name of Math.max(StudentAverage[].average)
+// (1147) first part done! next: how to access name tied to max of student_averages
+// (1148) maybe: sort student_averages by average, then access last element?
+// (1151) okay, looks like return type is satisfied; save and test?
+// (1153) lmao we close with just indexing,,,
+export function findTopStudent(students: Student[]): string {
+  const student_averages = students.map((student) => {
+    return ({
+      name: student.name,
+      average: student.scores.reduce((prev, current) => prev + current, 0) / student.scores.length
+    })
+  })
+
+  const sorted_averages = student_averages.sort((a, b) => a.average - b.average)
+
+  return sorted_averages[-1].name
 }
