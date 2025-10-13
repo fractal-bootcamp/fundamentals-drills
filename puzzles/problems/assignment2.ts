70339058dd44ce26fda77f27328ec08d3b56d3ab
Isaac Restrick <isaac.restrick@gmail.com>
2025-10-13 14:53:01 -0400

    Assignment 2

--- puzzles/problems/assignment2.ts
+++ puzzles/problems/assignment2.ts
@@ -26,7 +26,72 @@
  * Request: { customerId: "customer1", row: 0, seatsNeeded: 2 }
  * Result: Seats 0-1 in row 0 reserved, seats 3-4 still available
  */
+type Request = {
+  customerId: string,
+  row: number,
+  seatsNeeded: number
+}
 
-export function processReservations(initialTheater, requests): any {
-  //TODO
+type Response = {
+  customerId: string,
+  row: number,
+  startSeat: number
+  endSeat: number,
+}
+
+type Result = {
+  successfulReservations: Array<Response>,
+  finalTheater: Array<Array<string>>
+}
+
+export function processReservations(initialTheater: Array<Array<string>>, requests: Array<Request>): Result {
+  if (initialTheater.length === 0) {
+    return {
+      successfulReservations: [],
+      finalTheater: []
+    }
+  }
+
+  let theater: Array<Array<string>> = initialTheater
+  let reservations: Array<Response> = []
+  const rowLength: number = theater[0].length
+
+  for (const request of requests) {
+    if (!(0 <= request.row && request.row < rowLength)) continue
+
+    // Find the leftmost available seat, if there is one
+    let leftmostSeat = -1
+    for (let col = 0; col < rowLength; col++) {
+      let enoughSeats = true;
+      for (let j = col; j < col + request.seatsNeeded; j++) {
+        if (theater[request.row][j] !== "A") {
+          enoughSeats = false;
+        }
+      }
+      if (enoughSeats) {
+        leftmostSeat = col
+        break
+      }
+    }
+
+    // are the seats available?
+    if (leftmostSeat === - 1) continue
+
+    // if seats are available, reserve them!
+    for (let col = leftmostSeat; col < leftmostSeat + request.seatsNeeded; col++) 
+      theater[request.row][col] = "R"
+
+    // and add the reservation too!
+    reservations.push({
+      customerId: request.customerId,
+      row: request.row,
+      startSeat: leftmostSeat,
+      endSeat: leftmostSeat + request.seatsNeeded -1,
+    })
+  }
+
+  return {
+    successfulReservations: reservations,
+    finalTheater: theater
+  }
 }
