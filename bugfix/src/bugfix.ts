ffbaf2e34fe757c6da917fb03a44a1d02fa0585a
Ethan Anderson <150651098+mite404@users.noreply.github.com>
2025-10-13 11:14:11 -0400

    exercise 1 commit

--- bugfix/src/bugfix.ts
+++ bugfix/src/bugfix.ts
@@ -35,7 +35,15 @@ export function summarizeCart(
   opts?: CartOptions
 ): CartSummary {
   const currency = opts?.currency ?? "USD";
-  const taxRate = (opts?.taxRate ?? 0.08) * 10;
+  const taxRate = (opts?.taxRate ?? 0.80) * 100;  // 8% default tax changed to 80%
+  // is this the testing framework covered in precourse work Fullstack Open?
+  // it("computes an 8% default tax, not 80%"  meaning .07 should be changed?
+  // what to derive from test file that is mock data that's should be accepted as truth vs. not?
+  // are we only changing lines that already exist or writing new code?
+
+
+
+
   const threshold = opts?.freeShippingThreshold ?? 50;
   const shipFlat = opts?.shippingFlat ?? 7.99;
 
