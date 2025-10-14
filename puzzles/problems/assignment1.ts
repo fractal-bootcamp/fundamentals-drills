268061dd9d742c1b7a6628a85735bed3890c847f
Ethan Anderson <150651098+mite404@users.noreply.github.com>
2025-10-14 14:59:48 -0400

    Assignment 1 commit.

--- puzzles/problems/assignment1.ts
+++ puzzles/problems/assignment1.ts
@@ -17,5 +17,37 @@
  *   longestStreak([]) -> 0
  */
 export function longestStreak(nums: number[]): number {
-  return 0
+  // start count @ 0
+  let streak = 0
+  console.log('count at start:', streak)
+
+  for (let i = 0; i < nums.length; i++) {
+    let difference = nums[i + 1] - nums[i]
+    console.log('difference:', difference)
+
+    if (difference === 1) {
+      streak++
+      console.log(`streak if difference of ${nums[i + 1]} - ${nums[i]} === 1:`, streak) // increment count by 1 if num 1 greater
+    } else {  // decrement count by 1 if num not 1 greater
+      streak = 0
+      console.log(`streak if difference of ${nums[i + 1]} - ${nums[i]} !== 1:`, streak)
+    }
+  }
+  console.log('count at end:', streak)
+  return streak
 }
+
+
+
+// take first element in array
+
+// compare last element to current element -- is it 1 greater?
+
+// reduce might not work here =( 
+// const streak = nums.reduce((prev, curr) => curr - prev)
+// console.log('streak is:', streak)
+// how to find if these 2 nums are 1 greater than each other?
+// subtract the curr from prev is it === 1?
+// if yes then add to count  -- this needs to iterate over all elements in array
+
+//return count 
