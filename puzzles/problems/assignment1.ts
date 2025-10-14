ddecf1f352f6d09d76d1fc3f7f98a2904293ab6f
kudzuweb <144738515+kudzuweb@users.noreply.github.com>
2025-10-14 14:50:37 -0400

    puzzle 1

--- puzzles/problems/assignment1.ts
+++ puzzles/problems/assignment1.ts
@@ -17,5 +17,43 @@
  *   longestStreak([]) -> 0
  */
 export function longestStreak(nums: number[]): number {
-  return 0
+  let prevInteger = nums[0];
+  let oldCount = 0;
+  let newCount = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    let currentInteger = i;
+
+    if (currentInteger > prevInteger && currentInteger < prevInteger + 2) {
+      newCount += 1
+
+    }
+    else oldCount = newCount;
+    newCount = 0;
+  }
+
+  return (oldCount)
 }
+
+//pseudo
+// let prevInteger = 0
+// let oldCount = 0
+
+// for each integer in array
+// currentInteger = i
+// newCount = 0
+// if currentInteger > prevInteger && currentInteger < prevInteger + 2,
+// then newCount += 1
+// else oldCount = newCount
+//  newCount = 0
+// return oldCount
+
+// sketch
+// each time the consecutive int is +1, counter up 1
+// if next int is >+1 or <+1, store count and restart counter
+// if newCount > oldCount, oldCount = newCount, newCount = 0
+// return newCount
+// i think i need to actually start the comparison backwards? store first int,
+// then move to second int and compare second to first. how to represent that the value is
+// greater by exactly 1? greater than first int && less than second int+2
+
