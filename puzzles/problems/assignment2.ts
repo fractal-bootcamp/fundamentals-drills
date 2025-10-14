f3538933b31bb655a0d24b434bb6e3eb3487c1d4
Isaac Restrick <isaac.restrick@gmail.com>
2025-10-14 14:57:42 -0400

    ???

--- puzzles/problems/assignment2.ts
+++ puzzles/problems/assignment2.ts
@@ -55,6 +55,141 @@
  *     ]
  */
 
-export function processVendingSessions(input) {
-  return {}
+type Input = {
+  inventory: { [sku: string]: { price: number; stock: number } },
+  sessions: Array<Session>
+}
+
+type Session = Action[]
+
+type Action = ["insert", number] | ["select", string] | ["cancel"] | ["noop"]
+
+type Output = {
+  inventory: { [sku: string]: { price: number; stock: number } },
+  receipts: Array<{
+    dispensed?: string;
+    changeCoins: { [denom: number]: number };
+    changeTotal: number;
+    spent: number;
+    errors: string;
+  }>
+}
+
+export function processVendingSessions(input: Input): Output {
+  const denominations: Array<number> = [100, 50, 25, 10, 5, 1]
+  let receipts: Array<{
+    dispensed?: string;
+    changeCoins: { [denom: number]: number };
+    changeTotal: number;
+    spent: number;
+    errors: string;
+  }> = []
+  
+  let inserted: Map<number, number> = {
+    100: 0,
+    50: 0,
+    25: 0,
+    10: 0,
+    5: 0,
+    1: 0
+  }
+
+  let credit: number = 0;
+  let spent: number = 0;
+  let errors: Array<string> = []
+  
+  for (const session of input.sessions) {
+    if (session[0] === "insert") {
+      if (denominations.contains(session[1])) {
+        credit += session[1]
+        inserted.set(session[1], inserted.get(session[1]) + 1)
+      } else {
+        errors.push(`unsupported coin: ${session[1]}`)
+      }
+    } else if (session[0] === "select") {
+      if (!input.inventory.keys().contains(session[1])) {
+        errors.push(`invalid sku: ${session[1]}`)
+      } else if (input.inventory.get(session[1]).stock <= 0) {
+        errors.push(`out of stock: ${session[1]}`)
+      } else if (credit < input.inventory.get(session[1]).price) {
+        errors.push(`insufficient credit: have ${credit}, need ${input.inventory.get(session[1]).price}`)
+      } else {
+        input.inventory.get(session[1]).stock -= 1
+        spent += input.inventory.get(session[1]).price
+        let changeTotal: number = credit - price
+        // handle change
+        let changeCoins: Map<number, number> = {
+          100: 0,
+          50: 0,
+          25: 0,
+          10: 0,
+          5: 0,
+          1: 0
+        }
+
+        while (changeTotal > 0) {
+          let coinToRemove: number = 1
+          for (const coin of denominations) {
+            if (coin < changeTotal) {
+              coinToRemove = coin
+              break
+            }
+          }
+          changeTotal -= coinToRemove
+
+          if (changeCoins.get(coinToRemove)) {
+            changeCoins.set(coinToRemove, changeCoins.get(coinToRemove) + 1)
+          } else {
+            changeCoins.set(coinToRemove, 1)
+          }
+        }
+
+        receipts.push({
+          dispensed: session[1],
+          changeCoins: changeCoins,
+          changeTotal: changeTotal,
+          spent: spent,
+          errors: errors
+        })
+      }
+    } else if (session[0] === "cancel") {
+      let changeCoins: Map<number, number> = {}
+      let changeTotal: number = 0
+      for (const coin in inserted) {
+        if (inserted.get(coin) > 0) {
+          changeCoins.set(coin, inserted.get(coin))
+          changeTotal += coin * inserted.get(coin)
+        }
+      }
+      return {
+        inventory: input.inventory,
+        receipts: [{
+          dispensed: undefined,
+          changeCoins: changeCoins,
+          changeTotal: changeTotal,
+          spent: spent,
+          errors: errors
+        }]
+      }
+    } else if (session[0] === "noop") {
+      // nothing ever happens
+    }
+  }  
+  if (receipts.length === 0) {
+    return {
+      inventory: input.inventory,
+      receipts: {
+        dispensed: undefined,
+        changeCoins: {},
+        changeTotal: 0,
+        spent: spent,
+        errors: errors
+      }
+  }
+  } else {
+    return {
+      inventory: input.inventory,
+      receipts: receipts
+    }
+  }
 }
