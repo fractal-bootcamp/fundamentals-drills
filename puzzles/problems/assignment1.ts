9a7abb1783e7d11c9f8d73de4cb6f63c84dc62e7
genesisdayrit <genesisdayrit@gmail.com>
2025-10-14 14:58:32 -0400

    assignment 1 attempt

--- puzzles/problems/assignment1.ts
+++ puzzles/problems/assignment1.ts
@@ -16,6 +16,58 @@
  *   longestStreak([5,5,5]) -> 1
  *   longestStreak([]) -> 0
  */
+
+// check the array
+// hold a previous value that starts off as undefined
+// hold a value for streak counter, starts off as 0
+// for each number in the array, 
+// check if the value is equal to 1 + previous value
+// if so, then increment the streak counter by 1
+// if not, reset the streak counter to 0
+// set the current value to the previous value
+
+
+type ComparisonValue = number | undefined
+
 export function longestStreak(nums: number[]): number {
-  return 0
+  
+  let comparisonValue: ComparisonValue = undefined
+  let streakCounter: number = 1
+  let streakArray: number[] = []
+  let longestStreakValue: number = 0
+
+  // if nums.length === 0 then push 0
+
+  for (let i = 0; i < nums.length; i++) {
+    let currentValue = nums[i]
+    
+    if (!comparisonValue) {
+        console.log('No comparison value')
+        streakArray.push(streakCounter)
+        comparisonValue = currentValue
+    } else if (currentValue === comparisonValue + 1) {
+      streakCounter += 1
+      comparisonValue = currentValue
+      console.log(`Streak is now ${streakCounter} at position index ${i} (value: ${currentValue}, comparison value ${comparisonValue}`)
+    } else if (currentValue !== comparisonValue + 1) {
+        streakArray.push(streakCounter)
+        comparisonValue = currentValue
+        streakCounter = 1
+        console.log(`Streak reset at position index ${i} (value: ${currentValue}, comparison value ${comparisonValue}`)
+    // } else if (currentValue !== comparisonValue + 1 && streakCounter <= 1) {
+    //   streakCounter = 1  
+    //   break
+    }
+  }
+
+  streakArray.push(streakCounter)
+
+
+  for (let j = 0; j < streakArray.length; j++) {
+    if (streakArray[j] >= longestStreakValue) {
+      longestStreakValue = streakArray[j]
+      console.log('Longest Streak:', longestStreakValue)
+    } 
+  }
+  return longestStreakValue
 }
