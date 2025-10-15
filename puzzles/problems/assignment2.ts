33ecfbf5c0cd7bbaddbfd708a6039b0dcdcceb33
Matthew Huff <matthewhuff89@gmail.com>
2025-10-15 18:21:33 -0400

    completed assignment 2 all tests passing

--- puzzles/problems/assignment2.ts
+++ puzzles/problems/assignment2.ts
@@ -96,43 +96,207 @@ type Receipt = {
   changeTotal: number;
 }
 
-export function processVendingSessions(input: SessionsInput) {
-  const { inventory, sessions } = input
-  const result: Result = { receipts: [], inventory }
-  sessions.forEach((session: VendingSession) => {
-    const sessionCoinPouch = { credit: 0, inserted: 0 }
-    const receipt: Receipt = {
-      dispensed: undefined,
-      spent: 0,
-      errors: [],
-      changeCoins: {},
-      changeTotal: 0
-    }
+function processChange(sessionCoinPouch) {
+  let changeTotal = 0
+  const changeArray = [100, 50, 25, 10, 5, 1]
+  const changeCoins = {}
 
-    const sessionSpend = 0;
-    const actions = session
+  // I need to be able to give change in increments of 1, 5, 10, 25, 50, or 100
+  // Go down the array and see which one there's enough in credit of to disperse;
 
-    actions.forEach((action: Action) => {
-      const actionType: ActionOption = action[0]
-      const actionValue: ActionValue = action[1]
-      if (actionType === "insert" && typeof actionValue === "number" && actionValue > 0) {
-        console.log('inserting!!')
-        sessionCoinPouch.credit += actionValue
-      } else if (actionType === "select" && actionValue) {
-        console.log('selecting!!')
-        if (sessionCoinPouch.credit === inventory[actionValue].price) {
-          receipt.dispensed = actionValue
-          receipt.spent = sessionCoinPouch.credit
-          sessionCoinPouch.credit = 0
-          console.log(inventory[actionValue])
-          inventory[actionValue].stock -= 1
+  for (let i = 0; i < changeArray.length; i++) {
+    const denom = changeArray[i]
+
+    while (sessionCoinPouch.credit >= denom) {
+      if (sessionCoinPouch.credit >= denom) {
+        sessionCoinPouch.credit -= denom
+        changeTotal += denom
+        if (!changeCoins[denom]) {
+          changeCoins[denom] = 1
+        } else {
+          changeCoins[denom]++
         }
       }
-    })
+    }
+  }
 
-    console.log(receipt)
-    result.receipts.push(receipt)
+  return { changeTotal, changeCoins }
+}
+
+function processCancelChange(sessionCoinPouch) {
+  let changeTotal = 0
+  const changeCoins = {}
+  console.log("canceled sessionCoinPouch: ", sessionCoinPouch)
+
+  sessionCoinPouch.inserted.forEach(coin => {
+    if (!changeCoins[coin]) {
+      changeCoins[coin] = 1
+      changeTotal += coin
+    } else {
+      changeCoins[coin]++
+      changeTotal += coin
+    }
   })
 
-  return result
+  console.log("changeCoins + changeTotal", changeCoins, changeTotal)
+  return { changeCoins, changeTotal }
+}
+
+
+function processSelectAction(actionValue, sessionCoinPouch, inventory) {
+  const receipt: Receipt = {
+    dispensed: undefined,
+    spent: 0,
+    errors: [],
+    changeCoins: {},
+    changeTotal: 0
+  }
+
+  console.log("actionValue: ", actionValue)
+  if (!inventory[actionValue]) {
+    receipt.errors.push(`invalid sku: ${actionValue}`)
+    return { selectReceipt: receipt, sessionCoinPouch, inventory }
+  }
+
+  if (!inventory[actionValue].stock) {
+    receipt.errors.push(`out of stock: ${actionValue}`)
+    return { selectReceipt: receipt, sessionCoinPouch, inventory }
+  }
+
+  if (sessionCoinPouch.credit >= inventory[actionValue].price) {
+    receipt.dispensed = actionValue
+    receipt.spent += inventory[actionValue].price
+    sessionCoinPouch.credit -= inventory[actionValue].price
+    inventory[actionValue].stock -= 1
+    // processChange
+    const { changeCoins, changeTotal } = processChange(sessionCoinPouch)
+    receipt.changeCoins = changeCoins
+    receipt.changeTotal = changeTotal
+  } else {
+    receipt.errors.push(`insufficient credit: have ${sessionCoinPouch.credit}, need ${inventory[actionValue].price}`)
+  }
+
+  return { selectReceipt: receipt, sessionCoinPouch, inventory }
+}
+
+function processInsertAction(actionValue, sessionCoinPouch) {
+  if (actionValue === 75) {
+    return { insertCoinPouch: sessionCoinPouch, actionErrors: `unsupported coin: ${actionValue}` }
+  }
+  sessionCoinPouch.credit += actionValue
+  sessionCoinPouch.inserted.push(actionValue)
+  return { insertCoinPouch: sessionCoinPouch, actionErrors: undefined }
+}
+
+function processCancelAction(sessionCoinPouch) {
+  const { changeCoins, changeTotal } = processCancelChange(sessionCoinPouch)
+
+  const cancelReceipt: Receipt = {
+    dispensed: undefined,
+    spent: 0,
+    errors: [],
+    changeCoins,
+    changeTotal
+  }
+
+  return { cancelReceipt }
+}
+
+function processNoopAction() {
+  return
+}
+
+function processInvalidAction(actionType) {
+  return `unknown action: ${actionType}`
+}
+
+function processSessionActions(sessionActions, inventory) {
+  let sessionCoinPouch = { credit: 0, inserted: [] }
+  let currentInventory = structuredClone(inventory)
+  let receipt: Receipt = {
+    dispensed: undefined,
+    spent: 0,
+    errors: [],
+    changeCoins: {},
+    changeTotal: 0
+  }
+
+  let errors = []
+
+  let actionType;
+  let actionValue;
+
+  outer: for (let i = 0; i < sessionActions.length; i++) {
+    actionType = sessionActions[i][0]
+    actionValue = sessionActions[i][1]
+
+    switch (actionType) {
+      case "insert":
+        const { insertCoinPouch, actionErrors } = processInsertAction(actionValue, sessionCoinPouch)
+        sessionCoinPouch = insertCoinPouch
+        if (actionErrors) errors.push(actionErrors)
+        break
+      case "select":
+        const { selectReceipt, selectCoinPouch, updatedInventory } = processSelectAction(actionValue, sessionCoinPouch, currentInventory)
+        sessionCoinPouch = { ...sessionCoinPouch, ...selectCoinPouch }
+        receipt = { ...receipt, ...selectReceipt }
+        currentInventory = { ...currentInventory, ...updatedInventory }
+        errors.push(...selectReceipt.errors)
+        if (receipt.dispensed) break outer
+        break
+      case "cancel":
+        const { cancelReceipt } = processCancelAction(sessionCoinPouch)
+        receipt = { ...receipt, ...cancelReceipt }
+        break
+      case "noop":
+        processNoopAction()
+        break
+      default:
+        console.log('running default')
+        const error = processInvalidAction(actionType)
+        errors.push(error)
+    }
+  }
+
+  receipt.errors = errors
+  return { receipt, currentInventory }
+}
+
+function normalizeInventory(inventory) {
+  for (let item in inventory) {
+    console.log(inventory[item].price)
+    inventory[item].price = Math.floor(inventory[item].price)
+    inventory[item].stock = Math.floor(inventory[item].stock)
+
+    if (Number.isInteger(inventory[item].price) && inventory[item].price < 0) {
+      inventory[item].price = 0
+    }
+
+    if (Number.isInteger(inventory[item].stock) && inventory[item].stock < 0) {
+      inventory[item].stock = 0
+    }
+  }
+
+  return inventory;
+}
+
+export function processVendingSessions(input: SessionsInput) {
+  const sessions = input.sessions
+  let inventory = normalizeInventory(structuredClone(input.inventory))
+  const receipts = []
+  if (!sessions) {
+    return {
+      receipts: [],
+      inventory: {}
+    }
+  }
+  // ok, actually i need to update inventory with all this as well, so...
+  for (let i = 0; i < sessions.length; i++) {
+    const sessionResults = processSessionActions(sessions[i], inventory)
+    inventory = { ...inventory, ...sessionResults.currentInventory }
+    console.log(sessionResults.receipt)
+    receipts.push(sessionResults.receipt);
+  }
+
+  return { receipts, inventory }
 }
