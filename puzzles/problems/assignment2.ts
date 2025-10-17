976fde1e22352b8cdb25c7b6add6f7a430a72441
genesisdayrit <genesisdayrit@gmail.com>
2025-10-17 15:08:17 -0400

    assignment 2 exercise. passing 7 of 9 at 1hr

--- puzzles/problems/assignment2.ts
+++ puzzles/problems/assignment2.ts
@@ -58,6 +58,161 @@
  *    ]
  *    ⇒ active: { x:{enteredAt:"A"} }
  */
+
+/*
+
+problem intuition:
+
+riders are going in and out of a subway turnstile
+
+*/
+
+
+
+type Event = {
+  id: string
+  action: 'enter' | 'exit'
+  station: string
+}
+type Active = {}
+type Completed = Event[]
+type Stats = {
+  entries: {}
+  exits: {}
+}
+
+type Result = {
+}
+
 export function processTurnstileTrips(events: Event[]) {
-  return {} // TODO
+
+
+  let result = []
+  let active = {}
+  let completed = []
+  let rejected = []
+  let entries = {}
+  let exits = {}
+  
+  // if no events return result
+  if (events.length === 0) {
+    result = {
+      active: active,
+      completed: completed,
+      rejected: rejected,
+      stats: {
+        entries: entries,
+        exits: exits
+      }
+    }
+  }
+
+
+  for (let i = 0; i < events.length; i++) {
+    const event = events[i]
+    const { id, action, station } = event
+    console.log('Event processing:', event)
+
+    // if missing required entry field, skip
+    if (!id || !action || station) {
+      console.log('Invalid entry, missing entry field')
+      // skip to the next event if undefined!
+    }
+    
+    // if action is enter, then add to entries
+    if (action === "enter") {
+      // push the station to the entries object
+      // if the property exists, add it
+      const stationEntryExists = Object.hasOwn(entries, station)
+      console.log('station entry exists:', stationEntryExists);
+      
+      // station doesn't exist add it + 1 entry
+      if (!stationEntryExists) {
+        console.log('adding station to entries')
+        const addEntryValue = 1
+        entries[station] = addEntryValue
+        console.log('new entry object: ', entries)
+      
+      }
+
+      // check if there is an active entry in the active object
+      const activEntryExists = Object.hasOwn(active, id)
+      console.log('active entry exists:', activEntryExists);
+      
+      // if active entry doesn't exist, then add it to the active object
+      if (!activEntryExists) {
+        active[id] = { enteredAt: station}
+        console.log('new active object:', active)
+      } else {
+        let rejectObject = {
+          id: id, 
+          action: action, 
+          station: station, 
+          reason: "already in-system"
+        }
+
+        rejected.push(rejectObject)
+      }
+
+      
+    }
+
+    // if action is enter, then add to entries
+    if (action === "exit") {
+      // push the station to the entries object
+      // if the property exists, add it
+      const stationExitExists = Object.hasOwn(exits, station)
+      console.log('station exit exists:', stationExitExists);
+      
+      // station doesn't exist add it + 1 entry
+      if (!stationExitExists) {
+        console.log('adding station to entries')
+        const addExitValue = 1
+        exits[station] = addExitValue
+        console.log('new exit object: ', exits)
+      }
+      
+      // if there is an active id, remove it from the active object
+      const activEntryExists = Object.hasOwn(active, id)
+      console.log('active entry found for exit event:', activEntryExists)
+      
+
+      if (activEntryExists) {
+        const fromStation = active[id].enteredAt
+        console.log('from station:', fromStation)
+        const toStation = station
+        delete active[id]
+        console.log('new active object after an exit', active)
+
+        let completedTrip = {
+          id: id, 
+          from: fromStation, 
+          to: toStation
+        }
+        completed.push(completedTrip)
+      } else {
+        let rejectObject = {
+          id: id, 
+          action: action, 
+          station: station, 
+          reason: "not in-system"
+        }
+        rejected.push(rejectObject)
+      }
+    }
+
+    result = {
+      active: active, 
+      completed: completed,
+      rejected: rejected,
+      stats: {
+        entries: entries,
+        exits: exits
+      }
+    }
+
+    console.log('final result:', result)
+  }
+
+  return result
 }
