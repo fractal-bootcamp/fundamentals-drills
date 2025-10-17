97451f63b3f9cdbffa07896bca955669237bd671
alakn-eng <narayanan.aartil@gmail.com>
2025-10-17 15:10:06 -0400

    assignment 2

--- puzzles/problems/assignment2.ts
+++ puzzles/problems/assignment2.ts
@@ -58,6 +58,115 @@
  *    ]
  *    ⇒ active: { x:{enteredAt:"A"} }
  */
-export function processTurnstileTrips(events: Event[]) {
-  return {} // TODO
+
+
+type Event = {
+  id: string
+  action: "enter" | "exit"
+  station: string
+}
+
+type Active = Record<string, { enteredAt: string }>;
+
+type Completed = {
+  id: string
+  from: string
+  to: string
+}
+
+type Rejected = { //reason: "not in-system" or "already in-system"
+  id: string
+  action: "enter" | "exit";
+  reason: string
+}
+
+type EnterExitStat = Record<string, number>;
+
+type riderOutput = {
+  active: Active
+  completed: Completed[]
+  rejected: Rejected[]
+  stats: {
+    entries: EnterExitStat
+    exits: EnterExitStat
+  }
+}
+
+
+//determine if a rider is active
+function checkIfRiderActive(activeRides: Active, rider: string): boolean {
+  if (activeRides[rider]) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+
+
+
+export function processTurnstileTrips(events: Event[]): riderOutput {
+
+  let activeRides: Active = {}
+  let rejectedRides: Rejected[] = []
+  let completedRides: Completed[] = []
+  let tempReject: Rejected = {} //temp rejected object
+  let tempCompleted: Completed = {}
+  let entryStats: EnterExitStat = {}
+  let exitStats: EnterExitStat = {}
+
+
+  let output: riderOutput = {
+    activeRides,
+    completedRides, rejectedRides,
+    stats: { entryStats, exitStats }
+  }
+
+
+
+  for (let e of events) {
+    console.log("current action:", e.action)
+    if (e.action == "enter") { //if its enter, check if the rider is in the system
+      if (!checkIfRiderActive(activeRides, e.id)) {
+        //TODO: check if station is a non-empty string
+        activeRides[e.id] = e.station //add active rider station to active property
+        entryStats[e.id] += 1;
+
+      }
+      else {
+        tempReject.id = e.id;
+        tempReject.action = e.action;
+        tempReject.reason = "not in system"
+        rejectedRides.push(tempReject); //push the rejected object into rejected array
+
+      }
+      //if the rider isnt in the system, add entry into rejected array
+    }
+    else if (e.action == "exit") {
+      //helper fn to determine if the rider is an active rider
+      if (checkIfRiderActive(activeRides, e.id)) {
+
+        // update completedRides
+        tempCompleted.id = e.id;
+        tempCompleted.from = activeRides[e.id] //starting location
+        tempCompleted.to = e.station; //ending location
+        completedRides.push(tempCompleted)
+        //TODO update exitStats
+        exitStats[e.id] += 1;
+        //TODO remove from activeRides
+        delete activeRides[e.id]
+      }
+
+    }
+    else {
+      console.log("invalid event, do nothing")
+    }
+  }
+
+  console.log(output)
+
+
+
+  return output // TODO
 }
