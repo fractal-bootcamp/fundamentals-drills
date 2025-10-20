662c9b8bd384d4af4c0dfb2074c93d10deb82bb7
Ethan Anderson <150651098+mite404@users.noreply.github.com>
2025-10-19 20:12:50 -0400

    Finished the initial batch of Saturday practice problems

--- puzzles/problems/assignment1.ts
+++ puzzles/problems/assignment1.ts
@@ -1,66 +1,69 @@
-
 // given an array, return `true` if the largest number in the array is even. examples:
 // [1,2,113,8,42,111,10,3,523,6,226,7,7,7,227,87,7,7,7,743,7,7,34,34,43,3,343,3,333,3,323,3,3,3,5,6,7,8,1,3,435] => false
 // [2,45,6,6,6,6,6,10] => true
 // [1] => false
 
-
-const myArr = [1, 2, 113, 8, 42, 111, 10, 3, 523, 6, 226, 7, 7, 7, 227, 87, 7, 7, 7, 743, 7, 7, 34, 34, 43, 3, 343, 3, 333, 3, 323, 3, 3, 3, 5, 6, 7, 8, 1, 3, 435]
+const myArr = [
+  1, 2, 113, 8, 42, 111, 10, 3, 523, 6, 226, 7, 7, 7, 227, 87, 7, 7, 7, 743, 7, 7, 34, 34,
+  43, 3, 343, 3, 333, 3, 323, 3, 3, 3, 5, 6, 7, 8, 1, 3, 435,
+];
 function isLargestNumberEven(arr: number[]): boolean {
-  let biggestNumberSeenSoFar = arr[0]
+  let biggestNumberSeenSoFar = arr[0];
 
   for (const currentWorkingNumber of arr) {
     if (currentWorkingNumber > biggestNumberSeenSoFar) {
-      biggestNumberSeenSoFar = currentWorkingNumber
-      console.log(biggestNumberSeenSoFar)
+      biggestNumberSeenSoFar = currentWorkingNumber;
+      console.log(biggestNumberSeenSoFar);
     }
   }
 
-  let remainder = biggestNumberSeenSoFar % 2
+  const remainder = biggestNumberSeenSoFar % 2;
 
   if (remainder == 0) {
-    return true
-
-  } else return false
+    return true;
+  } else return false;
 }
 
 // console.log(isLargestNumberEven(myArr))
 
-
 // given an array of student scores, replace the "points" and "maxPossiblePoints" with a percentage grade instead. examples:
 // [{ student: "steve", points: 50, maxPossiblePoints: 100}] => [ { student: "steve", grade: .5 }]
-// [{ student: "bob", points: 50, maxPossiblePoints: 75}, { student: "joe", points: 1, maxPossiblePoints: 10}] => 
+// [{ student: "bob", points: 50, maxPossiblePoints: 75}, { student: "joe", points: 1, maxPossiblePoints: 10}] =>
 //    [ { student: "steve", grade: .6666666... }, { student: "joe", grade: .1 }]
 
-type StudentInput = [{
-  student: string
-  points: number
-  maxPossiblePoints: number
-}]
+type StudentInput = [
+  {
+    student: string;
+    points: number;
+    maxPossiblePoints: number;
+  },
+];
 
 // this is the OUTPUT
-type PercentageGrades = Student[]
+type PercentageGrades = Student[];
 
 type Student = {
-  student: string
-  grade: number
-}
+  student: string;
+  grade: number;
+};
 
-function convertToPercentages(scores: { student: string, points: number, maxPossiblePoints: number }[]): PercentageGrades {
-  const studentGrades: PercentageGrades = []
+function convertToPercentages(
+  scores: { student: string; points: number; maxPossiblePoints: number }[],
+): PercentageGrades {
+  const studentGrades: PercentageGrades = [];
 
   for (const data of scores) {
-    const percentageGrade = data.points / data.maxPossiblePoints
+    const percentageGrade = data.points / data.maxPossiblePoints;
 
     const studentGradeObj = {
       student: data.student,
-      grade: percentageGrade
-    }
+      grade: percentageGrade,
+    };
 
-    studentGrades.push(studentGradeObj)
+    studentGrades.push(studentGradeObj);
   }
 
-  return studentGrades
+  return studentGrades;
 }
 
 // const testArr = [{ student: "steve", points: 50, maxPossiblePoints: 100 }]
@@ -70,29 +73,31 @@ function convertToPercentages(scores: { student: string, points: number, maxPoss
 // [{ name: "oreo", pelt: "tuxedo", eyes: "green"}, {name: "tiger", pelt: "orange", eyes: blue }] => ["tiger"]
 // [{ name: "kitty", pelt: "orange", eyes: "red"}] => []
 type CatType = {
-  name: string
-  pelt: string
-  eyes: string
-}
+  name: string;
+  pelt: string;
+  eyes: string;
+};
 
-type CatResults = string[]
-
+type CatResults = string[];
 
 function findBlueEyedCats(cats: CatType[]): string[] {
-  const eyeColorArr: CatResults = []
+  const eyeColorArr: CatResults = [];
 
   for (const data of cats) {
-    if (data.eyes === 'blue') {
-      const catResultObj = data.name
-      eyeColorArr.push(catResultObj.toString())
+    if (data.eyes === "blue") {
+      const catResultObj = data.name;
+      eyeColorArr.push(catResultObj.toString());
     }
   }
 
-  return eyeColorArr
+  return eyeColorArr;
 }
 
-const myCatsArr = [{ name: "oreo", pelt: "tuxedo", eyes: "green" }, { name: "tiger", pelt: "orange", eyes: 'blue' }]
-console.log(findBlueEyedCats(myCatsArr))
+const myCatsArr = [
+  { name: "oreo", pelt: "tuxedo", eyes: "green" },
+  { name: "tiger", pelt: "orange", eyes: "blue" },
+];
+// console.log(findBlueEyedCats(myCatsArr));
 
 // given an array of numbers, return true if the array of numbers strictly decreases. examples:
 // [5,4,3,1] => true
@@ -100,8 +105,26 @@ console.log(findBlueEyedCats(myCatsArr))
 // [5,4,3,2,3] => false
 // [9,10,8,5,4,3] => false
 
+// type InputArr = number[];
+// let isDecreasingArr = [5, 4, 3, 1];
+// let isDecreasingArr = [99, 43, 10];
+// let isDecreasingArr = [5, 4, 3, 2, 3];
+let isDecreasingArr = [9, 10, 8, 5, 4, 3];
+
 function isDecreasing(input: number[]): boolean {
+  let prevNum = input[0];
 
+  for (let i = 0; i < input.length; i++) {
+    const currentNum = input[i];
 
-  return false
+    if (currentNum > prevNum) {
+      return false;
+    }
+
+    prevNum = currentNum;
+  }
+
+  return true;
 }
+
+console.log(isDecreasing(isDecreasingArr));
